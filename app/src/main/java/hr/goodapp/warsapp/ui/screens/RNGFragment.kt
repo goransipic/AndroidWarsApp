b2b29b9a6cef0532d34e4ee3731961ea0b969638
goransipic <goran.sipic@akd.hr>
2020-09-05 13:50:01 +0200

    RNG screen handling return result

--- app/src/main/java/hr/goodapp/warsapp/ui/screens/RNGFragment.kt
+++ app/src/main/java/hr/goodapp/warsapp/ui/screens/RNGFragment.kt
@@ -3,15 +3,34 @@ package hr.goodapp.warsapp.ui.screens
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import hr.goodapp.warsapp.R
+import hr.goodapp.warsapp.databinding.RngfragmentBinding
+import hr.goodapp.warsapp.ui.common.BaseFragment
+import hr.goodapp.warsapp.ui.common.Event
 import hr.goodapp.warsapp.ui.viewmodels.RNGViewModel
+import hr.goodapp.warsapp.ui.common.viewBinding
+import hr.goodapp.warsapp.ui.screens.ValidationFragment.Companion.RETURN_RESULT
 
-class RNGFragment : Fragment(R.layout.rngfragment) {
+class RNGFragment : BaseFragment(R.layout.rngfragment) {
 
     private lateinit var viewModel: RNGViewModel
+    private val viewBinding by viewBinding(RngfragmentBinding::bind)
+    private val args by navArgs<RNGFragmentArgs>()
+    private lateinit var savedStateHandle: SavedStateHandle
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        savedStateHandle = findNavController().previousBackStackEntry!!.savedStateHandle
 
+        viewBinding.textView2.text = args.inputText.toString()
+
+        viewBinding.button.setOnClickListener {
+            savedStateHandle.set(RETURN_RESULT, "Return from RNG")
+            findNavController().popBackStack()
+        }
     }
 
 }
--- app/src/main/java/hr/goodapp/warsapp/ui/screens/ValidationFragment.kt
+++ app/src/main/java/hr/goodapp/warsapp/ui/screens/ValidationFragment.kt
@@ -2,17 +2,27 @@ package hr.goodapp.warsapp.ui.screens
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.observe
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import hr.goodapp.warsapp.R
 import hr.goodapp.warsapp.databinding.ValidationFragmentBinding
 import hr.goodapp.warsapp.ui.common.BaseFragment
+import hr.goodapp.warsapp.ui.common.Event
+import hr.goodapp.warsapp.ui.common.EventObserver
 import hr.goodapp.warsapp.ui.common.viewBinding
 import hr.goodapp.warsapp.ui.viewmodels.ValidationViewModel
 
 
 class ValidationFragment : BaseFragment(R.layout.validation_fragment) {
+
+    companion object {
+        const val RETURN_RESULT: String = "RETURN_RESULT"
+    }
 
     private val viewModel  by viewModels<ValidationViewModel>()
     private val viewBinding by viewBinding(ValidationFragmentBinding::bind)
@@ -24,7 +34,7 @@ class ValidationFragment : BaseFragment(R.layout.validation_fragment) {
         }
 
         viewBinding.rng.setOnClickListener {
-            findNavController().navigate(R.id.action_validationFragment_to_RNGFragment)
+            findNavController().navigate(ValidationFragmentDirections.actionValidationFragmentToRNGFragment(viewBinding.textInputLayout.editText?.text.toString()))
         }
 
         viewBinding.language.setOnClickListener {
@@ -36,5 +46,21 @@ class ValidationFragment : BaseFragment(R.layout.validation_fragment) {
         }
 
         viewModel.refreshLanguage()
+
+        listenForResult()
+
+    }
+
+    private fun listenForResult() {
+        val navController = findNavController()
+
+        val currentBackStackEntry = navController.currentBackStackEntry!!
+        val savedStateHandle = currentBackStackEntry.savedStateHandle
+
+        savedStateHandle.getLiveData<String>(RETURN_RESULT)
+            .observe(currentBackStackEntry, Observer { success ->
+                Toast.makeText(requireContext(), success, Toast.LENGTH_SHORT).show()
+                savedStateHandle.remove<String>(RETURN_RESULT)
+            })
     }
 }
